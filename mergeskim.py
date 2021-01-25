7bb0db149d2cb2e03ce463569879df03e0ceb607
Andrzej <novak5andrzej@gmail.com>
2021-01-25 16:43:45 +0100

    feat: fix for files with variable number of branches

---
+++ mergeskim.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+import argparse
+import sys
+import os
+import ROOT
+ROOT.gROOT.SetBatch()
+ROOT.gErrorIgnoreLevel = ROOT.kError
+#import time
+
+# This is list of variables (nVar have to be first)
+varsToSave = [
+    'nElectron',
+    'nFatJet',
+    'nGenPart',
+    'nJet',
+    'nMuon',
+    'nSubJet',
+    'nTau',
+    'Electron_cutBased',
+    'Electron_eta',
+    'Electron_pt',
+    'FatJet_btagDDBvLV2',
+    'FatJet_btagDDCvBV2',
+    'FatJet_btagDDCvLV2',
+    'FatJet_eta',
+    'FatJet_jetId',
+    'FatJet_n2b1',
+    'FatJet_particleNetMD_Xbb',
+    'FatJet_particleNetMD_Xcc',
+    'FatJet_particleNet_HbbvsQCD',
+    'FatJet_particleNet_HccvsQCD',
+    'FatJet_phi',
+    'FatJet_pt',
+    'FatJet_subJetIdx1',
+    'FatJet_subJetIdx2',
+    'GenPart_eta',
+    'GenPart_genPartIdxMother',
+    'GenPart_pdgId',
+    'GenPart_phi',
+    'GenPart_pt',
+    'GenPart_statusFlags',
+    'HLT_AK8DiPFJet280_200_TrimMass30_BTagCSV_p20',
+    'HLT_AK8PFHT700_TrimR0p1PT0p03Mass50',
+    'HLT_AK8PFHT800_TrimMass50',
+    'HLT_AK8PFJet330_PFAK8BTagCSV_p17',
+    'HLT_AK8PFJet330_TrimMass30_PFAK8BoostedDoubleB_np4',
+    'HLT_AK8PFJet360_TrimMass30',
+    'HLT_AK8PFJet400_TrimMass30',
+    'HLT_AK8PFJet420_TrimMass30',
+    'HLT_AK8PFJet500',
+    'HLT_Mu50',
+    'HLT_PFHT1050',
+    'HLT_PFHT650_WideJetMJJ900DEtaJJ1p5',
+    'HLT_PFHT800',
+    'HLT_PFHT900',
+    'HLT_PFJet450'
+    'HLT_PFJet500',
+    'HLT_TkMu50',
+    'Jet_btagDeepB',
+    'Jet_eta',
+    'Jet_hadronFlavour',
+    'Jet_jetId',
+    'Jet_phi',
+    'Jet_pt',
+    'MET_pt',
+    'Muon_eta',
+    'Muon_looseId',
+    'Muon_pfRelIso04_all',
+    'Muon_phi',
+    'Muon_pt',
+    'Pileup_nPU',
+    'SubJet_eta',
+    'SubJet_mass',
+    'SubJet_phi',
+    'SubJet_pt',
+    'SubJet_rawFactor',
+    'Tau_idDecayMode',
+    'Tau_pt',
+    'event',
+    'genWeight',
+    'luminosityBlock',
+    'run', 
+]
+
+def doFiles(filelist, out):
+    # Group by because Run2016B doesn't have all the same branches
+    blist_counts = {}
+    blist_available = {}
+    for f in filelist:
+        rf = ROOT.TFile.Open(f)
+        to_save = [n.GetName() for n in rf.Get('Events').GetListOfBranches() if n.GetName() in varsToSave]
+        blist_counts[f] = len(to_save)
+        blist_available[len(to_save)] = to_save
+    grouped = {}
+    for key, value in sorted(blist_counts.items()):
+        grouped.setdefault(value, []).append(key)
+
+    # Actual skimming
+    outs = []
+    for i, (branch_number, fileset) in enumerate(grouped.items()):
+        out_name = out.rstrip(".root")+f"_{i}.root"
+        outs.append(out_name)
+        if os.path.isfile(out_name):
+            continue
+        else:
+            fChain = ROOT.TChain("Events")   
+            for f in fileset:
+                fChain.Add(f)
+            available = [branch.GetName() for branch in fChain.GetListOfBranches()]
+            isData = 'nGenPart' not in available
+
+            events_df = ROOT.RDataFrame(fChain)
+            # Pre-selection
+            # sel = "Sum(FatJet_pt>350)>0"
+            sel = "2+2 == 4"
+            events_fil0 = events_df.Filter(sel)
+            # More filters can be added if needed
+            events_out = events_fil0
+
+            outVars = ROOT.std.vector('string')()
+            for v in blist_available[branch_number]:
+                #if v in available:
+                outVars.push_back(v)
+
+            events_out.Snapshot("Events", out_name, outVars)
+
+            fChain.Reset()
+            del fChain
+            del events_df
+            del events_out
+            del outVars
+    print(outs) # print output names, to be collected by parsl
+    return 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Run Skim and add trees for a sample', usage="./SkimTrees.py sample_name")
+    parser.add_argument('out', type=str)
+    parser.add_argument('filenames', metavar='FILES', type=str, nargs='+',
+                        help='an integer for the accumulator')
+    args = parser.parse_args()
+
+    # TH1DModel = ROOT.RDF.TH1DModel
+    # ROOTDataFrame = ROOT.RDataFrame
+    # usingRDF = True
+    # ROOT.ROOT.EnableImplicitMT(1)
+
+    #start = time.time()
+    doFiles(args.filenames, args.out)
+    #end = time.time()
+    #print("TIME:", time.strftime("%H:%M:%S", time.gmtime(end-start)))
--- skimmer.py
+++ skimmer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+import ast
 import json
 import time
 import argparse
@@ -33,13 +34,11 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--dir', help='Output directory', required=True)
     parser.add_argument('-o', '--output', default=r'dummy_skim.json', help='')
     parser.add_argument('--limit', type=int, default=None, help='')
-    parser.add_argument('-m', type=int, default=500,
+    parser.add_argument('-m', type=int, default=2000,
                         help="MB size of input files to merge")
-    parser.add_argument('-n', type=int, default=None,
-                        help="Number of files to merge")
     parser.add_argument('-j', '--ncpu', type=int, default=1,
                         help="Number of CPUs to use with RDataFrame")
-    parser.add_argument('--run', help='Bool', action='store_true')
+    parser.add_argument('--run', action='store_true', help='Bool')
     args = parser.parse_args()
 
     start = time.time()
@@ -49,10 +48,10 @@ if __name__ == "__main__":
 
     # Hadd instance
     @bash_app
-    def skim_files(filelist=[], out='hadded.root', ith=None):
+    def skim_files(filelist=[], out='hadded.root', ith=None, stderr=parsl.AUTO_LOGNAME, stdout=parsl.AUTO_LOGNAME):
         if ith is not None:
             print(ith)
-        return f'python -b skimhadd.py {out} {" ".join(filelist)}'
+        return f'python -b mergeskim.py {out} {" ".join(filelist)}'
 
 
     print("Storage dir:")
@@ -64,9 +63,9 @@ if __name__ == "__main__":
 
     # Write futures
     out_dict = {} # Output filename list
-    run_futures = [] # Future list
+    run_futures = {} # Future list
     for key in sorted(sample_dict.keys()):
-        new_list = [] 
+        run_futures[key] = []
         # Make size batches
         batches = []
         batch_list = []
@@ -74,34 +73,45 @@ if __name__ == "__main__":
         for i, fname in enumerate(sample_dict[key]):
             batch_size += os.stat(fname).st_size
             batch_list.append(fname)
-            if batch_size > args.m*1024*1024:
+            # batch end conditions:
+            _batch_size = batch_size > args.m*1024*1024
+            _batch_end = i == len(sample_dict[key]) - 1
+            _batch_run_boundary = False
+            if len(batch_list) > 0 and not _batch_end:
+                _batch_run_boundary = fname.split("/Run20")[-1][:3] != sample_dict[key][i+1].split("/Run20")[-1][:3]
+            if _batch_size or _batch_end or _batch_run_boundary:
                 batches.append(batch_list)
                 tot_size += batch_size
                 batch_size = 0
                 batch_list = []
 
+        # print(key, len(sample_dict[key]), len(batches), len(sum(batches, [])))
+        if len(sample_dict[key]) != len(sum(batches, [])):
+            raise ValueError('Number of inputs files is not the same as batched total.')
         for i, fnames in enumerate(batches):
             if len(fnames) == 0:
                 continue
             if i%5 == 0: 
-                # print some progress info
+                # generate some progress strings
                 ith = f'{key}: {i}/{len(batches)} ({sizeof_fmt(tot_size)})'
-                print("TIME:", time.strftime("%H:%M:%S", time.gmtime(time.time()-start)))
             else:
                 ith = None
             outpath = os.path.join(os.path.abspath(args.dir), key.strip('/'))
             Path(outpath).mkdir(parents=True, exist_ok=True)
             out = os.path.join(outpath, fnames[0].split("/")[-1].lstrip("/"))
-            new_list.append(out)
-            if os.path.isfile(out):
-                'File found'
-            else: 
-                x = skim_files(filelist=fnames, out=out, ith=ith)
-                run_futures.append(x)
-        out_dict[key] = new_list
+            x = skim_files(filelist=fnames, out=out, ith=ith)
+            run_futures[key].append(x)
+        
 
-    for i, r in enumerate(run_futures):
-        r.result()
+    if args.run:
+        for key, future_list in run_futures.items():
+            new_list = []
+            for i, r in enumerate(future_list):
+                r.result()
+                with open(r.stdout, 'r') as f:
+                    new_list.extend(ast.literal_eval(f.read()))
+            out_dict[key] = new_list
+        
 
     print("Writing files to {}".format(args.output))
     with open(args.output, 'w') as fp:
